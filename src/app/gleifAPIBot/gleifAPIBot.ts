aefbe35896baac53c8ecb32f23058386c1525ae6
reaas <reaas@stud.ntnu.no>
2020-09-10 10:36:11 +0200

    Initial working version for GLEIF API

---
+++ src/app/gleifAPIBot/gleifAPIBot.ts
@@ -0,0 +1,92 @@
+import {
+  TeamsActivityHandler,
+  TurnContext,
+  MessageFactory,
+  CardFactory,
+  MessagingExtensionAction,
+  MessagingExtensionActionResponse,
+  MessagingExtensionAttachment,
+  MessagingExtensionQuery,
+  MessagingExtensionResponse,
+  Attachment
+} from 'botbuilder';
+
+import * as Util from "util";
+const TextEncoder = Util.TextEncoder;
+
+import * as debug from "debug";
+const log = debug("msteams");
+
+export class GleifAPIBot extends TeamsActivityHandler {
+  private APIURL: string = "https://api.gleif.org/api/v1";
+
+  constructor() {
+    super();
+  }
+
+  protected async handleTeamsMessagingExtensionQuery(
+    context: TurnContext,
+    query: MessagingExtensionQuery
+  ): Promise<MessagingExtensionResponse> {
+    let searchQuery = "";
+
+    if (query &&
+      query.parameters &&
+      query.parameters[0].name === "searchQuery" &&
+      query.parameters[0].value
+    ) {
+      searchQuery = query.parameters[0].value.trim().toLowerCase();
+    }
+
+    return new Promise((resolve, reject) => {
+      fetch(this.APIURL + "/fuzzycompletions?field=fulltext&q=" + searchQuery, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then((results) => results.json())
+      .then(async (data) => {
+        console.log('wat');
+        const searchResultsCards = this.getCompanyResultCards(data.data);
+
+
+        setTimeout(() => {
+          console.log('sesarch: ', searchResultsCards)
+          resolve({
+            composeExtension: {
+              type: "result",
+              attachmentLayout: "list",
+              attachments: searchResultsCards
+            }
+          });
+        }, 1000)
+
+        
+      })
+      .catch((error) => reject("Error getting gleif companies: " + error))
+    });
+  }
+
+  private getCompanyResultCards(companies: any[]): Attachment[] {
+    let cards: Attachment[] = [];
+
+    companies.forEach((company) => {
+      if (company.relationships) {
+        fetch(company.relationships["lei-records"].links.related, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then((result) => result.json())
+        .then(({ data }) => {
+          cards.push(CardFactory.heroCard(company.attributes.value, data.id));
+        })
+        .catch((error) => cards.push(CardFactory.heroCard("Error getting lei-records: " + error)));
+      }
+    });
+
+    return cards;
+  }
+}
