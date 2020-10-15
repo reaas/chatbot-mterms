36b9ff06177f4c1d628a2256cec63754f9d67014
Henrik <hgg1998@gmail.com>
2020-10-15 18:52:08 +0200

    Start graph connect.

--- src/app/personalChatBot/personalChatBot.ts
+++ src/app/personalChatBot/personalChatBot.ts
@@ -1,9 +1,26 @@
-import { CardFactory, TeamsActivityHandler } from "botbuilder";
+/*
+import {
+  Activity,
+  ActivityTypes,
+  BotState,
+	CardFactory,
+  ChannelAccount,
+  ConversationState,
+  Mention,
+  SigninStateVerificationQuery,
+  StatePropertyAccessor,
+  TeamsActivityHandler,
+  TurnContext,
+  UserState
+} from 'botbuilder';
 import * as debug from "debug";
 import * as AdaptiveCards from "adaptivecards";
 import { DBClient } from "../TeamsAppsComponents";
 import { BuyForm } from "../forms/buyForm";
 import { ISINCard } from "../cards/isinCard";
+import { Dialog, DialogState } from 'botbuilder-dialogs';
+import { MainDialog } from '../dialogs/mainDialog';
+
 const log = debug("msteams");
 
 export class PersonalChatBot extends TeamsActivityHandler {
@@ -115,3 +132,133 @@ export class PersonalChatBot extends TeamsActivityHandler {
     return textMessage;
   }
 }
+*/
+
+import {
+  Activity,
+  ActivityTypes,
+  BotState,
+  ChannelAccount,
+  ConversationState,
+  Mention,
+  SigninStateVerificationQuery,
+  StatePropertyAccessor,
+  TeamsActivityHandler,
+  TurnContext,
+  UserState
+} from 'botbuilder';
+import { Dialog, DialogState } from 'botbuilder-dialogs';
+import { MainDialog } from '../dialogs/mainDialog';
+
+export class PersonalChatBot extends TeamsActivityHandler {
+  private conversationState: BotState;
+  private userState: BotState;
+  private dialog: Dialog;
+  private dialogState: StatePropertyAccessor<DialogState>;
+  /**
+   *
+   * @param {ConversationState} conversationState
+   * @param {UserState} userState
+   * @param {Dialog} dialog
+   */
+  constructor(
+    conversationState: BotState,
+    userState: BotState,
+    dialog: Dialog
+  ) {
+    super();
+    if (!conversationState) {
+        throw new Error('[SimonBot]: Missing parameter. conversationState is required');
+    }
+    if (!userState) {
+        throw new Error('[SimonBot]: Missing parameter. userState is required');
+    }
+    if (!dialog) {
+        throw new Error('[SimonBot]: Missing parameter. dialog is required');
+    }
+    this.conversationState = conversationState as ConversationState;
+    this.userState = userState as UserState;
+    this.dialog = dialog;
+    this.dialogState = this.conversationState.createProperty<DialogState>('DialogState');
+
+    this.onMessage(async (context, next) => {
+      
+      // Run the Dialog with the new message Activity.
+      await (this.dialog as MainDialog).run(context, this.dialogState);
+
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+
+    this.onDialog(async (context, next) => {
+        // Save any state changes. The load happened during the execution of the Dialog.
+        await this.conversationState.saveChanges(context, false);
+        await this.userState.saveChanges(context, false);
+
+        // By calling next() you ensure that the next BotHandler is run.
+        await next();
+    });
+
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded!;
+      for (const member of membersAdded) {
+        if (member.id !== context.activity.recipient.id) {
+          // If we are in Microsoft Teams
+          if (context.activity.channelId === 'msteams') {
+            // Send a message with an @Mention
+            await this._messageWithMention(context, member);
+          } else {
+            // Otherwise we send a normal echo
+            await context.sendActivity(`Welcome to Simon Bot ${member.name}. This Bot is a work in progress. At this time we have some dialogs working. Type anything to get started.`);
+          }
+        }
+      }
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+
+    this.onTokenResponseEvent(async (context, next) => {
+      console.log('Running dialog with Token Response Event Activity.');
+
+      // Run the Dialog with the new Token Response Event Activity.
+      await (this.dialog as MainDialog).run(context, this.dialogState);
+
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+  });
+  }
+
+  /**
+   * Override the ActivityHandler.run() method to save state changes after the bot logic completes.
+   */
+  public async run(context): Promise<void> {
+    await super.run(context);
+
+    // Save any state changes. The load happened during the execution of the Dialog.
+    await this.conversationState.saveChanges(context, false);
+    await this.userState.saveChanges(context, false);
+  }
+  
+  public async handleTeamsSigninVerifyState(context: TurnContext, query: SigninStateVerificationQuery): Promise<void> {
+    await (this.dialog as MainDialog).run(context, this.dialogState);
+  }
+
+  private async _messageWithMention(context: TurnContext, member: ChannelAccount): Promise<void> {
+    // Create mention object
+    const mention: Mention = {
+        mentioned: member,
+        text: `<at>${member.name}</at>`,
+        type: 'mention'
+    };
+
+    // Construct message to send
+    const message: Partial<Activity> = {
+        entities: [mention],
+        text: `Welcome to Simon Bot ${mention.text}. This Bot is a work in progress. At this time we have some dialogs working. Type anything to get started.`,
+        type: ActivityTypes.Message
+    };
+
+    await context.sendActivity(message);
+  }
+}
+
--- src/app/server.ts
+++ src/app/server.ts
@@ -21,8 +21,11 @@ require("dotenv").config();
 
 // The import of components has to be done AFTER the dotenv config
 import * as allComponents from "./TeamsAppsComponents";
-import { BotFrameworkAdapter } from "botbuilder";
 import { PersonalChatBot } from "./personalChatBot/personalChatBot";
+import { BotFrameworkAdapter, ConversationState, MemoryStorage, UserState } from "botbuilder";
+
+// This bot's main dialog.
+import { MainDialog } from "./dialogs/mainDialog";
 
 // Create the Express webserver
 const express = Express();
@@ -86,10 +89,106 @@ botAdapter.onTurnError = async (context, error) => {
     await context.sendActivity("bot error");
 };
 
+
 // run the bot when messages are received on the specified path
-const bot = new PersonalChatBot();
+let conversationState: ConversationState;
+let userState: UserState;
+const memoryStorage = new MemoryStorage();
+conversationState = new ConversationState(memoryStorage);
+userState = new UserState(memoryStorage);
+const dialog = new MainDialog("mainDialog");
+const bot = new PersonalChatBot(conversationState, userState, dialog);
+
 express.post("/api/messages", (request, response) => {
     botAdapter.processActivity(request, response, async (context) => {
         await bot.run(context);
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import { BotFrameworkAdapter, ConversationState, MemoryStorage, UserState } from 'botbuilder';
+
+// This bot's main dialog.
+import { PersonalChatBot } from './PersonalChatBot/PersonalChatBot';
+import { MainDialog } from './dialogs/mainDialog';
+
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+// Create HTTP server.
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+});
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${ error }`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    // Clear out state
+    await conversationState.delete(context);
+};
+
+let conversationState: ConversationState;
+let userState: UserState;
+
+const memoryStorage = new MemoryStorage();
+conversationState = new ConversationState(memoryStorage);
+userState = new UserState(memoryStorage);
+
+const dialog = new MainDialog('mainDialog');
+const myBot = new PersonalChatBot(conversationState, userState, dialog);
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route to main dialog.
+        await myBot.run(context);
+    });
+});
+*/
