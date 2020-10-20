695e747c7cba60b7150fde95b5a12c7e001ec59e
Silje Leidland <Salja10@hotmail.com>
2020-10-20 16:06:20 +0200

    added helper files

---
+++ src/app/helper/simpleGraphClient.ts
@@ -0,0 +1,46 @@
+import { Client } from '@microsoft/microsoft-graph-client';
+import { User } from '@microsoft/microsoft-graph-types';
+
+export class SimpleGraphClient {
+    
+    private token: string;
+    private graphClient: Client;
+
+    constructor(token: any) {
+        if (!token || !token.trim()) {
+            throw new Error('SimpleGraphClient: Invalid token received.');
+        }
+
+        this.token = token;
+
+        this.graphClient = Client.init({
+            authProvider: (done) => {
+                done(null, this.token); 
+            }
+        });
+    }
+
+    /**
+     * Check if a user exists
+     * @param {string} emailAddress Email address of the email's recipient.
+     */
+    public async userExists(emailAddress: string): Promise<boolean> {
+        if (!emailAddress || !emailAddress.trim()) {
+            throw new Error('SimpleGraphClient.userExists(): Invalid `emailAddress` parameter received.');
+        }
+        try {
+            const user: User = await this.graphClient.api(`/users/${emailAddress}`).get();
+            return user ? true : false;
+        } catch (error) {
+            return false;
+        }
+    }
+    public async getUserID(): Promise<void> {
+        try {
+            const data = await this.graphClient.api('/planner/tasks').post({"planId":"ooju5jbJVU6QGW5aMiLTjZgAC5KZ","title":"New Task5","assignments":{}});
+            console.log("Fungerer", data)
+        } catch (error) {
+            console.log("her er feilen", error)
+        }
+    }
+}
