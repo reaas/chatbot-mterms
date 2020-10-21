289b09f55fb0efa2b6c1b51a8d9f06dbc8efbc6e
Hege <hegeaal@stud.ntnu.no>
2020-10-21 14:39:46 +0200

    bot creates task with description

--- src/app/helpers/simpleGraphClient.ts
+++ src/app/helpers/simpleGraphClient.ts
@@ -36,11 +36,27 @@ export class SimpleGraphClient {
         }
     }
     public async getUserID(): Promise<void> {
+        let taskID : String = ""
         try {
-            const data = await this.graphClient.api('/planner/tasks').post({"planId":"ooju5jbJVU6QGW5aMiLTjZgAC5KZ","title":"New Task5","assignments":{}});
-            console.log("Fungerer", data)
+            const data = await this.graphClient.api('/planner/tasks').post({"planId":"ooju5jbJVU6QGW5aMiLTjZgAC5KZ","title":"Bot task w/description 8)","assignments":{}});
+            //console.log("Fungerer", data)
+            taskID = data.id
         } catch (error) {
             console.log("her er feilen", error)
         }
-    }
+
+        try {
+            let details = await this.graphClient.api('/planner/tasks/' + taskID + "/details").get()
+            console.log("DETAILS", details)
+            let etag = details["@odata.etag"]
+            console.log("HER", etag)
+            const plannerTaskDetails = {description:"automatic description ;)"};
+            let res = await this.graphClient.api('/planner/tasks/' + taskID + '/details')
+            .header("If-Match", etag)
+            .update(plannerTaskDetails);
+            console.log("HER ER RES", res)
+        } catch (error) {
+            console.log("FEIL:", error)
+        }
+    } 
 }
